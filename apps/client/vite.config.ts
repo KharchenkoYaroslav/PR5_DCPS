89396663ad06737e159c98a119c47ff20b9b88e1
Yaroslav <kharchenko.yaroslav@lll.kpi.ua>
2025-05-28 17:42:36 +0300

    changed server address

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(() => ({
     host: 'localhost',
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://pr5-dcps.onrender.com',
         changeOrigin: true,
       },
     },
